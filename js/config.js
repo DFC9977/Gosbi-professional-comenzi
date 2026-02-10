84b53205c376efb23eec79f110cb5ef5a17e2580
DFC9977 <office@middas.ro>
2026-02-10 21:50:56 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "gosbiromania.firebaseapp.com",
+  projectId: "gosbiromania",
+  storageBucket: "gosbiromania.appspot.com",
+  messagingSenderId: "PASTE_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
